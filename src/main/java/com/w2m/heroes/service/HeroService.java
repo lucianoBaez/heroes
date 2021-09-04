bf3358f2bd98d0d79ce0ea5d1fb8b2f188628ace
Luciano Baez <luciano.baez@glic-solutions.com>
2021-09-04 15:54:12 -0300

    Agregando una cache a la busqueda del heroes, el tiempo de duracion se puede configurar en la propiedad: heroes.cacheExpiration

--- src/main/java/com/w2m/heroes/service/HeroService.java
+++ src/main/java/com/w2m/heroes/service/HeroService.java
@@ -2,6 +2,8 @@ package com.w2m.heroes.service;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.w2m.heroes.entity.Hero;
@@ -12,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
+@CacheConfig(cacheNames = "heroCache")
 public class HeroService {
 
    private final HeroRepository heroRepository;
@@ -31,6 +34,7 @@ public class HeroService {
       return hero;
    }
 
+   @Cacheable
    public List<Hero> findAll() {
       log.info("finding all users");
       return heroRepository.findAll();
